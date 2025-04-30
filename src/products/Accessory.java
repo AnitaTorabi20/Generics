05a94f6daebb287988bf7341cd01e3d81f68384b
Anita <anitatorabi.aut@gmail.com>
2025-04-30 10:07:09 +0330

    implement Accessory class

--- src/products/Accessory.java
+++ src/products/Accessory.java
@@ -1,4 +1,35 @@
 package products;
 
-public class Accessory {
+public class Accessory extends Product {
+    private String color;
+
+    private static int counter = 10;
+
+    public Accessory(String title, Double price, String color) {
+        super(title, price);
+        setId(generateId());
+        setColor(color);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color == null || color.trim().isEmpty()) {
+            throw new IllegalArgumentException("Color cannot be null or empty.");
+        }
+        this.color = color;
+    }
+
+    @Override
+    protected String generateId() {
+        return "3" + (counter++);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + " | Color: " + color;
+    }
 }
