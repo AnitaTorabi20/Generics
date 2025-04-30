4aa461f8f8f6f280313a934f55e0d700ee6d6b94
Anita <anitatorabi.aut@gmail.com>
2025-04-30 10:06:07 +0330

    implement Notebook.java

--- src/products/Notebook.java
+++ src/products/Notebook.java
@@ -1,4 +1,46 @@
 package products;
 
-public class Notebook {
+public class Notebook extends Product {
+    private int pageCount;
+    private boolean isHardCover;
+
+    private static int counter = 10;
+
+    public Notebook(String title, Double price, int pageCount, boolean isHardCover) {
+        super(title, price);
+        setId(generateId());
+        setPageCount(pageCount);
+        this.isHardCover = isHardCover;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        if (pageCount <= 0) {
+            throw new IllegalArgumentException("Page count must be greater than zero.");
+        }
+        this.pageCount = pageCount;
+    }
+
+    public boolean isHardCover() {
+        return isHardCover;
+    }
+
+    public void setHardCover(boolean hardCover) {
+        isHardCover = hardCover;
+    }
+
+    @Override
+    protected String generateId() {
+        return "2" + (counter++);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + " | Page Count: " + pageCount
+                + " | Hard Cover: " + (isHardCover ? "Yes" : "No");
+    }
 }
